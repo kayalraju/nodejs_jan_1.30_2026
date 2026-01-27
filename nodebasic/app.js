843dd9aad2e2d1ca33edbbc77bf3f2c4d3d31e56
kayalraju <kayalraju003@gmail.com>
2026-01-27 14:32:23 +0530

    mongodb connection

--- nodebasic/app.js
+++ nodebasic/app.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const express=require('express')
 const path=require('path')
 const ejs=require('ejs')
+const DatabaseConnection=require('./app/config/dbcon')
 
 
 // console.log(path);
 
 
 const app=express();
+
+//database connection
+DatabaseConnection()
 
 app.set('view engine','ejs')
 app.set('views','views')
